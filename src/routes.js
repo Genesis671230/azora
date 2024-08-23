4033b34842382f4ef7a9f68fbbd349406d13867b
HaM <97172297+Genesis671230@users.noreply.github.com>
2024-08-23 10:52:14 +0400

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,6 @@ import { MdContacts, MdHome, MdLeaderboard, MdLock } from "react-icons/md";
 // icon
 import React from "react";
 
-// const RevenueReportsView = React.lazy(() => import("features/revenue-report/RevenueReportView"));
-// Admin Imports
 const MainDashboard = React.lazy(() => import("views/admin/default"));
 
 // My component
@@ -26,73 +24,7 @@ const routes = [
     component: MainDashboard,
   },
 
-  // {
-  //   name: "Lead",
-  //   layout: "/admin",
-  //   both: true,
-  //   path: "/lead",
-  //   icon: <Icon as={MdLeaderboard} width='20px' height='20px' color='inherit' />,
-  //   component: Lead,
-  // },
-  // {
-  //   name: "Lead View",
-  //   layout: "/admin",
-  //   both: true,
-  //   under: "lead",
-  //   path: "/leadView/:id",
-  //   component: LeadView,
-  // },
 
-  // {
-  //   name: "Customers",
-  //   layout: "/admin",
-  //   both: true,
-  //   path: "/customers",
-  //   icon: <Icon as={MdContacts} width="20px" height="20px" color="inherit" />,
-  //   component: Contact,
-  // },
-  // {
-  //   name: "Contact View",
-  //   layout: "/admin",
-  //   both: true,
-  //   under: "contacts",
-  //   path: "/contactView/:id",
-  //   component: ContactView,
-  // },
-
-  // {
-  //   name: "Sales Order",
-  //   layout: "/admin",
-  //   both: true,
-  //   path: "/sales-order",
-  //   icon: <Icon as={LuBuilding2} width="20px" height="20px" color="inherit" />,
-  //   component: Properties,
-  // },
-  // {
-  //   name: "Quotations",
-  //   layout: "/admin",
-  //   both: true,
-  //   path: "/quotaions",
-  //   icon: <Icon as={GrMapLocation} width="20px" height="20px" color="inherit" />,
-  //   component: Location,
-  // },
-
-  // {
-  //   name: "Invoices",
-  //   layout: "/admin",
-  //   both: true,
-  //   path: "/invoices",
-  //   icon: <Icon as={LiaFileInvoiceSolid} width="20px" height="20px" color="inherit" />,
-  //   component: Invoices,
-  // },
-  // {
-  //   name: "Property View",
-  //   layout: "/admin",
-  //   both: true,
-  //   under: "properties",
-  //   path: "/propertyView/:id",
-  //   component: PropertyView,
-  // },
 
   {
     name: "Users",
@@ -103,14 +35,7 @@ const routes = [
   },
 
  
-  // {
-  //   name: "Deal View",
-  //   layout: "/admin",
-  //   both: true,
-  //   under: "deals",
-  //   path: "/dealView/:id",
-  //   component: PropertyView,
-  // },
+
 
   {
     name: "User View",
@@ -119,22 +44,7 @@ const routes = [
     path: "/userView/:id",
     component: UserView,
   },
-  // {
-  //   name: "Suppliers",
-  //   layout: "/admin",
-  //   both: true,
-  //   path: "/suppliers",
-  //   icon: <Icon as={GrProjects} width="20px" height="20px" color="inherit" />,
-  //   component: Project,
-  // },
-  // {
-  //   name: "Services",
-  //   layout: "/admin",
-  //   both: true,
-  //   path: "/services",
-  //   icon: <Icon as={HiOutlineDocumentReport } width="20px" height="20px" color="inherit" />,
-  //   component: RevenueReport,
-  // },
+
 
   {
     name: "Sign In",
